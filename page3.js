4bc80a178b3bf5d718e10faff7fd13df0bbb15f4
EricFreeman <gogosergey@gmail.com>
2019-06-27 15:59:57 +0300

    added proper redirect to page 3
    title

--- page3.js
+++ page3.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+var selectedcity;
 
+$(document).ready(function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+        } else {
+          // No user is signed in.
+          window.location.replace("login.html");
+        }
+      });
+    selectedcity= sessionStorage.getItem('city');
+    console.log("selected city is:",selectedcity);
+    console.log(firebase.auth().currentUser);
 });
